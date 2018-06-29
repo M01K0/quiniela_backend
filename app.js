786053d5a1fc4ac0c58872307d953ede61ddd70b
oscar cardenas <carde@MacBook-Pro-de-oscar.local>
2018-06-29 02:07:51 -0500

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 var app = express();
+
+
+app.use(cors())
 
 //cargar rutas
 var teamRoutes    = require('./routes/team');
@@ -13,13 +17,13 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //Configurar headers y cors
-app.use((req,res,next) => {
+/*app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Allow-Headers','Authorization, X-API-KEY, Origin, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
 
 //Rutas Base
 app.use('/api',teamRoutes);
